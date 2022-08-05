9cb35cf6ed113359c8d851c3a6fe99bedca8c3d7
gellyzxc <urasaftn@gmail.com>
2022-08-05 18:15:27 +0500

    time fixed

--- time.js
+++ time.js
@@ -1,6 +1,25 @@
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+if (today.getHours() < 10) {
+    hrsz = '0' + today.getHours();
+}   else {
+    hrsz = today.getHours();
+}
+
+if (today.getMinutes() < 10) {
+    minutesz = '0' + today.getMinutes();
+}   else {
+    minutesz = today.getMinutes();
+}
+
+if (today.getSeconds() < 10) {
+    secz = '0' + today.getSeconds();
+}   else {
+    secz = today.getSeconds();
+}
+
+var time = hrsz + ":" + minutesz + ":" + secz;
 var dateTime = date+' '+time;
 
 var h1 = document.getElementsByClassName('time')[0];
